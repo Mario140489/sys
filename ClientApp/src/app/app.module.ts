67df3215bd18049c4a9728c6c694a572b144c108
J.Mario Vieira <jm_bck@hotmail.com>
2019-08-30 11:24:12 -0300

    mudança de cores

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,11 +6,9 @@ import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TableBasicExample } from './listacontas/listaContas.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -19,8 +17,7 @@ import { MaterialFileInputModule } from 'ngx-material-file-input';
 import {MatCheckboxModule,
   MatTreeModule,
   MatSelectModule,MatRadioModule,
-  MatPaginatorModule, MatTooltipModule,
-  MatNativeDateModule, MatSortModule,
+  MatPaginatorModule, MatTooltipModule, MatSortModule,
   MatToolbarModule, MatButtonModule,
   MatSidenavModule, MatIconModule,
   MatGridListModule, MatListModule,
@@ -32,38 +29,18 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { NgxCurrencyModule } from "ngx-currency"
 import { MaisNavComponent } from './mais-nav/mais-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { ContasComponent } from './contas/contas.component';
-import { ListcontasComponent } from './listcontas/listcontas.component';
-import { ContasService } from './service/contas.service';
-import { CartoesService } from './service/cartoes.service';
-import { TpcontasService } from './service/tpcontas.service';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import br from '@angular/common/locales/br';
 import {NgxMaskModule} from 'ngx-mask-2'
-import { USE_VALUE } from '@angular/core/src/di/injector';
-import { TpcontasComponent } from './tpcontas/tpcontas.component';
-import { CartoesComponent } from './cartoes/cartoes.component';
-import { StatusComponent } from './status/status.component';
-import { ProdutogroupComponent } from './produtogroup/produtogroup.component';
-import { ListImobilizadoComponent } from './list-imobilizado/list-imobilizado.component';
 registerLocaleData(br, 'pt-BR');
 //import $ from "JQuery";
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    TableBasicExample,
     MaisNavComponent,
-    ContasComponent,
-    ListcontasComponent,
-    TpcontasComponent,
-    CartoesComponent,
-    StatusComponent,
-    ProdutogroupComponent,
-    ListImobilizadoComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -73,12 +50,6 @@ registerLocaleData(br, 'pt-BR');
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'listacontas', component: TableBasicExample },
-      { path: 'Status', component: StatusComponent },
-      { path: 'Cartoes', component:CartoesComponent},
-      { path: 'tp_contas', component:TpcontasComponent},
-      { path: 'Grupo', component:ProdutogroupComponent},
-      { path: 'imobilizado',component:ListImobilizadoComponent}
     ]),
     NgxMaskModule.forRoot(),
     MatTreeModule,
@@ -103,14 +74,13 @@ registerLocaleData(br, 'pt-BR');
     MatFormFieldModule,
     NgxCurrencyModule,
     MatDatepickerModule,
-    MatNativeDateModule,
     MatPaginatorModule,
     MatSortModule,
     MatTooltipModule,
     MatRadioModule,
     MaterialFileInputModule,
   ],
-  providers: [ContasService, TpcontasService,CartoesService, MatDatepickerModule, ListcontasComponent,
+  providers: [MatDatepickerModule,
     { provide: LOCALE_ID, useValue: "pt-br" }
   ],
   bootstrap: [AppComponent]
